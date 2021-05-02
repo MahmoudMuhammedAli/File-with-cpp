4edb5434372ed335d2821fc46e8fe6ecf2a06086
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-05-02 02:41:17 +0200

    ex1 done

---
+++ ex1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream> // contains file stream processing types
+#include <cstdlib> // exit function prototype
+using namespace std;
+int main()
+{
+    // ofstream constructor opens file
+    ofstream outClientFile("file_example.txt", ios::out);
+    // exit program if unable to create file
+    if (!outClientFile) // overloaded ! operator
+    {
+        cerr << "File could not be opened" << endl;
+        exit(EXIT_FAILURE);
+    } // end if
+    cout << "Enter the account, name, and balance." << endl
+         << "Enter end-of-file to end input.\n? "; // the account number
+    string firstName;
+    string lastName; // the account owner's name
+    int id;
+    string hash; // the account balance
+    // read account, name and balance from cin, then place in file
+    while (cin >> firstName >> lastName >> id >> hash)
+    {
+        outClientFile << firstName << ' ' << lastName << ' ' << id << ' ' << hash << endl;
+        cout << "? ";
+    } // end while
+} // end main
